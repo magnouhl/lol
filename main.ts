4f21d8423eaa87d396871c83f0ea926a1f443c37
magnouhl <64577633+magnouhl@users.noreply.github.com>
2021-01-12 13:30:55 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+    music.playMelody("E B C5 A B G A F ", 120)
+})
+basic.showString("HALLO ")
+basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
+})
